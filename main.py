8de86fcb7b7e8aaf49b47f1a6a3de4714b6e8ecf
Практикант 335 <Prakt335@npoa.org>
2022-02-04 12:02:46 +0500

    Улучшил метод crash_script

--- main.py
+++ main.py
@@ -205,6 +205,7 @@ class TrayIcon:
 
 def crash_script(error_type, value, tb):
     logging.critical(" {}   Название ошибки - {}, значение - {}, tb - {}".format(datetime.datetime.now().strftime('%H:%M:%S'), error_type, value, tb))
+    sys.__excepthook__(error_type, value, tb)
 
 
 def main():
