d4ca55664df1735bae6fd426ccbf4e5f7bee080f
Практикант 335 <Prakt335@npoa.org>
2022-02-07 12:04:56 +0500

    Исправил ошибку в тесте

--- tests/test_setting.py
+++ tests/test_setting.py
@@ -1,6 +1,5 @@
 import pytest
 import sys
-
 import yaml
 from PyQt5.QtWidgets import *
 sys.path.append('../')
@@ -19,6 +18,12 @@ class TestSetting:
         app = QApplication(sys.argv)
         app.setQuitOnLastWindowClosed(False)
         tray_icon = main.TrayIcon('../img/icon.png', app)
+        with open('conf.yaml') as f_obj:
+            read_data = yaml.load(f_obj, Loader=yaml.FullLoader)
+        read_data['server'] = 'server300:1090'
+        read_data['token'] = ''
+        with open('conf.yaml', 'w') as f_obj:
+            yaml.dump(read_data, f_obj)
         setting = main.Setting(tray_icon)
         setting.edit_token.setText('12345')
         setting.edit_server.setText('server300:1080')
