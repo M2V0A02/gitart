1b5536859872509aff1bc90d1a3b3830468f9364
Практикант 335 <Prakt335@npoa.org>
2022-02-07 15:25:30 +0500

    Убрал не нужную функцию и добавил комментарии

--- tests/test_api.py
+++ tests/test_api.py
@@ -5,11 +5,7 @@ sys.path.append('../')
 import main
 
 
-def test__init__creation_successfully():
-    main.Api('server300:1080', '9e8eab4fad52b94c1f57217e2cdfeea5a99229d7')
-    assert True
-
-
+# Передаю токен пользователя имеющий id - 2 и сервер, ожидаю что вернется  пользователь имеющий id - 2.
 def test_get_user_user_received():
     api = main.Api('server300:1080', '9e8eab4fad52b94c1f57217e2cdfeea5a99229d7')
     user = api.get_user()
@@ -17,12 +13,13 @@ def test_get_user_user_received():
     assert user_json['id'] == 2
 
 
+# Проверяю, что параметр сервер перезаписывается
 def test_set_server_server_received():
-    api = main.Api('server300:1080', '9e8eab4fad52b94c1f57217e2cdfeea5a99229d7')
+    api = main.Api('server300:1080', '')
     api.set_server('server300:1090')
     assert api.get_server() == 'server300:1090'
 
-
+# Проверяю что параметр токен перезаписывается
 def test_set_access_token_access_token_received():
     api = main.Api('server300:1080', '123')
     api.set_access_token('qwerty')
