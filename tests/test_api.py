fd9b2a488f584603387d8ff73fc3b3319cae21ba
Практикант 335 <Prakt335@npoa.org>
2022-02-07 11:43:00 +0500

    добавил проверку что токен изменяется

--- tests/test_api.py
+++ tests/test_api.py
@@ -8,7 +8,7 @@ import main
 class TestApi:
     def test__init__creation_successfully(self):
         main.Api('server300:1080', '9e8eab4fad52b94c1f57217e2cdfeea5a99229d7')
-        assert  True
+        assert True
 
     def test_get_user_user_received(self):
         api = main.Api('server300:1080', '9e8eab4fad52b94c1f57217e2cdfeea5a99229d7')
@@ -19,6 +19,9 @@ class TestApi:
     def test_set_server_server_received(self):
         api = main.Api('server300:1080', '9e8eab4fad52b94c1f57217e2cdfeea5a99229d7')
         api.set_server('server300:1090')
-        assert  api.get_server() == 'server300:1090'
+        assert api.get_server() == 'server300:1090'
 
-    
+    def test_set_access_token_access_token_received(self):
+        api = main.Api('server300:1080', '123')
+        api.set_access_token('qwerty')
+        assert api.get_access_token() == 'qwerty'
