04cd84e30827c7b3f14cbdc6ff335c80fdaddf8b
Практикант 335 <Prakt335@npoa.org>
2022-02-22 12:25:06 +0500

    Разделил метод show на более маленькие методы.

--- main.py
+++ main.py
@@ -33,38 +33,31 @@ class Notification:
         self.notification_ui = []
         self.api = api
 
-    def show(self, notifications):
-        self.notification_ui = []
-        self.layout = QVBoxLayout()
-        self.window = QWidget()
-        layout = QHBoxLayout()
-        label = QLabel("Непрочитанна - {} сообщений.".format(len(notifications)))
-        font = QtGui.QFont()
-        font.setPointSize(18)
-        label.setFont(font)
-        layout.addWidget(label)
-        button = QPushButton("Обновить")
-        button.setStyleSheet("max-width:75px; min-width:75px;")
-        button.clicked.connect(self.update)
-        layout.addWidget(button)
-        self.layout.addLayout(layout)
-        self.notification_ui.append(label)
+    def get_additional_information(self, notifications):
+        if not (notifications['subject']['latest_comment_url'] == ''):
+            return json.loads(self.api.get_comment(
+                re.search(r'comments/\d+', format(notifications['subject']['latest_comment_url']))[0].replace(
+                    'comments/',
+                    '')).text)
+        else:
+            repo = re.search(r'repos/.+/issues', notifications['subject']['url'])[0].replace('repos/', '').replace(
+                '/issues', '')
+            issues = re.search(r'/issues/.+', notifications['subject']['url'])[0].replace('/issues/', '')
+            notification = json.loads(self.api.get_repos_issues(repo, issues).text)
+            notification['body'] = 'новая задача'
+            return notification
+
+    def formatting_the_date(self, date):
+        date = datetime.datetime.strptime(date['created_at'], '%Y-%m-%dT%H:%M:%SZ')
+        timezone = str(datetime.datetime.now(datetime.timezone.utc).astimezone())
+        timezone = int(timezone[len(timezone) - 5:len(timezone) - 3])
+        date = date + datetime.timedelta(hours=timezone)
+        return date.strftime('%H:%M %d-%m-%Y')
+
+    def show_notifications(self, notifications):
         for i in range(len(notifications)):
-            if not (notifications[i]['subject']['latest_comment_url'] == ''):
-                notification = json.loads(self.api.get_comment(
-                    re.search(r'comments/\d+', format(notifications[i]['subject']['latest_comment_url']))[0].replace('comments/',
-                                                                                                            '')).text)
-            else:
-                repo = re.search(r'repos/.+/issues', notifications[i]['subject']['url'])[0].replace('repos/', '').replace(
-                    '/issues', '')
-                issues = re.search(r'/issues/.+', notifications[i]['subject']['url'])[0].replace('/issues/', '')
-                notification = json.loads(self.api.get_repos_issues(repo, issues).text)
-                notification['body'] = 'новая задача'
-            date = datetime.datetime.strptime(notification['created_at'], '%Y-%m-%dT%H:%M:%SZ')
-            timezone = str(datetime.datetime.now(datetime.timezone.utc).astimezone())
-            timezone = int(timezone[len(timezone) - 5:len(timezone) - 3])
-            date = date + datetime.timedelta(hours=timezone)
-            date = date.strftime('%H:%M %d-%m-%Y')
+            notification = self.get_additional_information(notifications[i])
+            date = self.formatting_the_date(notification)
             tasks = notifications[i]['subject']['title']
             if len(tasks) > 11:
                 tasks = "{}...".format(tasks[0:11])
@@ -89,9 +82,24 @@ class Notification:
             button.setStyleSheet(
                 "font-size:12px; color: #23619e; background: rgba(255,255,255,0); border-radius: .28571429rem; height: 20px; border-color: #dedede; text-align:right;")
             button.clicked.connect(open_notification)
-            button.setFont(font)
             self.layout.addWidget(button)
             self.notification_ui.append(button)
+
+    def create_window_notification(self, notifications):
+        self.notification_ui = []
+        self.layout = QVBoxLayout()
+        self.window = QWidget()
+        layout = QHBoxLayout()
+        label = QLabel("Не прочитано - {} сообщений.".format(len(notifications)))
+        label.setStyleSheet("font-size:24px;")
+        layout.addWidget(label)
+        button = QPushButton("Обновить")
+        button.setStyleSheet("max-width:75px; min-width:75px;")
+        button.clicked.connect(self.update)
+        layout.addWidget(button)
+        self.layout.addLayout(layout)
+        self.notification_ui.append(label)
+        self.show_notifications(notifications)
         self.layout.addStretch()
         self.window.setLayout(self.layout)
         self.scroll.setWidget(self.window)
@@ -103,7 +111,7 @@ class Notification:
                          int(screen_size[1] / 2) - int(window_size[1] / 2) - 20)
 
     def update(self):
-        self.show(self.tray.get_notifications())
+        self.create_window_notification(self.tray.get_notifications())
 
     def open_notification(self, url):
         logging.debug("Переход по ссылке - {}".format(url))
@@ -310,7 +318,7 @@ class TrayIcon:
         else:
             del self.window_notification
             self.window_notification = Notification(self.api, self)
-            self.window_notification.show(self.notifications)
+            self.window_notification.create_window_notification(self.notifications)
 
     def controller_tray_icon(self, trigger):
         if trigger == 3 and self.tray.authorization:  # Левая кнопка мыши
